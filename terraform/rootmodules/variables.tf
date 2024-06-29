c08919ccc171549b09d2437649513f315616f3df
zenitugo <ugochiukaegbu19@gmail.com>
2024-06-29 01:57:20 +0100

    variables holding all the resource values

---
+++ terraform/rootmodules/variables.tf
@@ -0,0 +1,12 @@
+variable "name" {}
+variable "location" {}
+variable "env" {}
+variable "address_space" {}
+variable "address_prefixes1" {}
+variable "address_prefixes2" {}
+variable "identity" {}
+variable "kubernetes_version" {}
+variable "node-group-name" {}
+variable "sku_tier" {}
+variable "service_cidr" {}
+variable "dns_service_ip" {}
